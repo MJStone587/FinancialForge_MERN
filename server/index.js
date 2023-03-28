edfbe853ede2264f224f218910878bba721c70af
MJStone587 <MJstone587@yahoo.com>
2023-03-28 11:23:33 -0500

    testing..

--- server/index.js
+++ server/index.js
@@ -6,7 +6,11 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors);
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
