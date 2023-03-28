2fa82b98bcf2cfd0301ac1a1dc5efdcaadce3e89
MJStone587 <MJstone587@yahoo.com>
2023-03-28 10:58:08 -0500

    re-apply cors

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const catalogRouter = require('./routes/catalog');
-//const cors = require('cors');
+const cors = require('cors');
 //""proxy": "http://localhost:5000", added to client package.json for local hosting
 require('dotenv').config();
 
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
-//app.use(cors);
+app.use(cors);
 
 const uri = process.env.ATLAS_URI;
 
