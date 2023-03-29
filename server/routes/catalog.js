a5517ee880f183b34a24d113220bb890b95e9274
MJStone587 <MJstone587@yahoo.com>
2023-03-29 11:09:10 -0500

    fixed catalog with put request

--- server/routes/catalog.js
+++ server/routes/catalog.js
@@ -15,14 +15,14 @@ router.post('/income/create', income_controller.income_create_post);
 //Delete Income
 router.delete('/income/:id', income_controller.income_delete);
 //Post request to update income
-router.post('/income/:id', income_controller.income_update);
+router.put('/income/:id', income_controller.income_update);
 
 //Get All Expenses
 router.get('/expense', expense_controller.expense_list);
 //Create new expense
 router.post('/expense/create', expense_controller.expense_create);
 //Update expense
-router.post('/expense/:id', expense_controller.expense_update);
+router.put('/expense/:id', expense_controller.expense_update);
 //Delete Expense
 router.delete('/expense/:id', expense_controller.expense_delete);
 
