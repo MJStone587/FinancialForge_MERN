35169faad09ad5ed436c8b3994da77c92aed22c1
MJStone587 <MJstone587@yahoo.com>
2023-04-28 10:11:54 -0500

    port change

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 10000;
+const port = process.env.PORT || 10000;
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
