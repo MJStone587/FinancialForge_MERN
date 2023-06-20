fdf9aa78e865fed9516ad26a4dd8c5270b5b4f0e
MJStone587 <MJstone587@yahoo.com>
2023-06-20 11:27:51 -0500

    server formatting option addition

--- server/models/income.js
+++ server/models/income.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const { DateTime } = require('luxon');
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 const opts = { toJSON: { virtuals: true } };
@@ -10,7 +10,7 @@ let IncomeSchema = new Schema(
     description: { type: String, required: true },
     category: {
       type: String,
-      enum: ['Job', 'Gift', 'Investment', 'Savings', 'Other'],
+      enum: ["Job", "Gift", "Investment", "Savings", "Other"],
       required: true,
     },
     dateReceived: { type: Date, required: true },
@@ -22,12 +22,16 @@ let IncomeSchema = new Schema(
   opts
 );
 
-IncomeSchema.virtual('date_received_med').get(function () {
+IncomeSchema.virtual("date_received_med").get(function () {
   return this.dateReceived.toLocaleDateString(DateTime.DATE_MED);
 });
 
-IncomeSchema.virtual('date_created_med').get(function () {
+IncomeSchema.virtual("date_received_med2").get(function () {
+  return this.dateReceived.toISODate();
+});
+
+IncomeSchema.virtual("date_created_med").get(function () {
   return this.dateCreated.toLocaleDateString(DateTime.DATE_MED);
 });
 
-module.exports = mongoose.model('Income', IncomeSchema);
+module.exports = mongoose.model("Income", IncomeSchema);
