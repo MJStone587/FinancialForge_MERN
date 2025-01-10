69ca6a50b025befcd7bc4780ec6b4cf5909fcbcd
MJStone587 <MJstone587@yahoo.com>
2025-01-10 14:50:18 -0600

    fixing movels/income.js VSCode changes

--- server/models/income.js
+++ server/models/income.js
@@ -5,39 +5,29 @@ const Schema = mongoose.Schema;
 const opts = { toJSON: { virtuals: true } };
 
 let IncomeSchema = new Schema(
-  {
-    name: { type: String, required: true },
-    description: { type: String, required: true },
-    category: {
-      type: String,
-      enum: ["Job", "Gift", "Investment", "Savings", "Other"],
-      required: true,
-    },
-    dateReceived: { type: Date, required: true },
-    dateCreated: { type: Date, default: () => Date.now() },
-    //author: { type: Schema.Types.ObjectId, ref: 'User' }, PREFERENCE?
-    total: { type: Number, required: true },
-    user_id: { type: String, required: true },
-  },
-  opts
+	{
+		name: { type: String, required: true },
+		description: { type: String, required: true },
+		category: {
+			type: String,
+			enum: ["Job", "Gift", "Investment", "Savings", "Other"],
+			required: true,
+		},
+		dateReceived: { type: Date, required: true },
+		dateCreated: { type: Date, default: () => Date.now() },
+		//author: { type: Schema.Types.ObjectId, ref: 'User' }, PREFERENCE?
+		total: { type: Number, required: true },
+		user_id: { type: String, required: true },
+	},
+	opts
 );
 
 IncomeSchema.virtual("date_received_med").get(function () {
-<<<<<<< HEAD
-  return this.dateReceived.toLocaleDateString(DateTime.DATE_FULL);
+	return this.dateReceived.toLocaleDateString(DateTime.DATE_FULL);
 });
 
-=======
-  return this.dateReceived.toLocaleDateString(DateTime.DATE_MED);
-});
-
-IncomeSchema.virtual("date_received_med2").get(function () {
-  return this.dateReceived.toISODate();
-});
-
->>>>>>> fdf9aa78e865fed9516ad26a4dd8c5270b5b4f0e
 IncomeSchema.virtual("date_created_med").get(function () {
-  return this.dateCreated.toLocaleDateString(DateTime.DATE_MED);
+	return this.dateCreated.toLocaleDateString(DateTime.DATE_MED);
 });
 
 module.exports = mongoose.model("Income", IncomeSchema);
